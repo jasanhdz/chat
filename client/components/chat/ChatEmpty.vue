30b480141896fad428eba2d6598fd9aa167c0d2a
Jasan Hernández <jasan814@gmail.com>
2024-11-10 17:52:24 -0600

    fix: update chatEmpty

--- client/components/chat/ChatEmpty.vue
+++ client/components/chat/ChatEmpty.vue
@@ -5,19 +5,14 @@
         class="mx-auto rounded-full bg-no-repeat bg-center bg-cover w-[250px] h-[250px]"
         :style="{ backgroundImage: `url(${require('app/assets/intro-connection-light.jpg')})` }"
       ></div>
-      <h1 class="mt-7 font-light text-gray-700">Mantén tu teléfono conectado</h1>
-      <p class="mt-4 text-sm leading-5 flex items-center justify-center">
-        WhatsApp se conecta a tu teléfono para sincronizar los mensajes. Para reducir el consumo de tus datos, conecta tu
-        teléfono a una red Wi-Fi.
-      </p>
+      <h1 class="mt-7 font-light text-gray-700">Selecciona un Usuario para comenzar a chatear</h1>
       <!-- Divider -->
       <div class="my-8 w-full border-b border-gray-300"></div>
       <p class="mt-4 text-sm leading-5 flex items-center justify-center">
         <span class="icon text-gray-500 text-xl mr-1">
           <DesktopTowerMonitorIcon width="21" height="21" />
         </span>
-        WhatsApp está disponible para Windows.
-        <span class="ml-1 text-green-500 cursor-pointer">Obtenlo aquí</span>
+        ChatAPP está disponible para chatear
       </p>
     </div>
     <!-- Línea en la parte inferior -->
